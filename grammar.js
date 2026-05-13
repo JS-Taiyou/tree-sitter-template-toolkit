6eec4d9fa4ffa4f00ad600dafc6ce8ffdd278117
Jorge Solís <jpsb23@gmail.com>
2026-05-13 12:36:59 -0600

    grammar fixes for comments, assignments, blocks and filters

--- grammar.js
+++ grammar.js
@@ -1,30 +1,36 @@
 module.exports = grammar({
   name: "template_toolkit",
 
-  extras: $ => [/\s/, $.comment],
+  extras: $ => [/\s/],
+
+  conflicts: $ => [[$.filter_start_directive, $.keyword], [$.elsif_clause], [$.else_clause], [$.variable, $.path], [$.path]],
 
   rules: {
     source_file: $ => repeat($._statement),
     _statement: $ => choice($.content, $.directive),
     content: $ => choice(/[^\[]+/, /\[/),
-    directive: $ => choice($.conditional_block, $.foreach_block, $.simple_directive),
-    comment: $ => /#.*/,
+    directive: $ => choice($.conditional_block, $.foreach_block, $.filter_block, $.simple_directive),
+    comment: $ => /#[^%]*/,
     simple_directive: $ => seq('[%', $._statement_list, '%]'),
     conditional_block: $ => seq(
       $.conditional_start_directive,
       repeat($._statement),
-      repeat($.elsif_directive),
-      optional($.else_directive),
+      repeat($.elsif_clause),
+      optional($.else_clause),
       $.end_directive
     ),
     foreach_block: $ => seq($.foreach_directive, repeat($._statement), $.end_directive),
+    filter_block: $ => seq($.filter_start_directive, repeat($._statement), $.end_directive),
     foreach_directive: $ => seq('[%', 'FOREACH', field('iterator', $.variable), field('operator', choice('=', 'IN')), field('list', $._value_expression), '%]'),
-    conditional_start_directive: $ => seq('[%', choice('IF', 'UNLESS'), $._value_expression, optional(seq(';', $._statement_list)), '%]'),
-    elsif_directive:             $ => seq('[%', 'ELSIF', $._value_expression, optional(seq(';', $._statement_list)), '%]'),
-    else_directive:              $ => seq('[%', 'ELSE', $._statement_list, '%]'),
-    end_directive:               $ => seq('[%', 'END', '%]'),
+    conditional_start_directive: $ => seq('[%', choice('IF', 'UNLESS'), $._value_expression, '%]'),
+    filter_start_directive: $ => seq('[%', 'FILTER', $._value_expression, '%]'),
+    elsif_directive: $ => seq('[%', 'ELSIF', $._value_expression, '%]'),
+    elsif_clause: $ => seq($.elsif_directive, repeat($._statement)),
+    else_clause: $ => seq($.else_directive_marker, repeat($._statement)),
+    else_directive_marker: $ => seq('[%', 'ELSE', '%]'),
+    end_directive: $ => seq('[%', 'END', '%]'),
     _statement_list: $ => seq($._directive_statement, repeat(seq(';', $._directive_statement))),
-    _directive_statement: $ => choice($.command_expression, $.assignment_expression, $._value_expression),
+    _directive_statement: $ => choice($.command_expression, $.assignment_expression, $.comment, $._value_expression),
     _value_expression: $ => choice(
       $.ternary_expression,
       $.binary_expression,
@@ -36,7 +42,7 @@ module.exports = grammar({
     primary_expression: $ => choice(
       prec(1, $.variable),
       $.string,
-      $.bare_string,
+      $.number,
       $.array,
       $.hash,
       $.parenthesized_expression
@@ -53,8 +59,9 @@ module.exports = grammar({
       field('if_false', $._value_expression)
     )),
     parenthesized_expression: $ => seq('(', $._statement_list, ')'),
-    command_expression: $ => seq($.keyword, repeat1($._value_expression)),
-    keyword: $ => choice('INCLUDE', 'USE', 'SET', 'GET', 'CALL', 'NEXT'),
+    command_expression: $ => seq($.keyword, repeat1(choice($.named_argument, $.path, $._value_expression))),
+    keyword: $ => choice('INCLUDE', 'USE', 'SET', 'GET', 'CALL', 'NEXT', 'FILTER'),
+    named_argument: $ => seq($.identifier, '=', $._value_expression),
     binary_expression: $ => choice(
       prec.left(3, seq(field('left', $._value_expression), field('operator', '_'), field('right', $._value_expression))),
       prec.left(2, seq(field('left', $._value_expression), field('operator', $.logical_op_high), field('right', $._value_expression))),
@@ -62,16 +69,17 @@ module.exports = grammar({
       prec.left(0, seq(field('left', $._value_expression), field('operator', $.comparison_operator), field('right', $._value_expression)))
     ),
     assignment_expression: $ => prec.right(0, seq($.variable, '=', $._value_expression)),
-    filter_expression: $ => seq($._value_expression, '|', $.identifier),
+    filter_expression: $ => prec.left(1, seq($._value_expression, '|', choice($.identifier, seq($.identifier, $.argument_list)))),
     string: $ => seq("'", /[^']*/, "'"),
-    array: $ => seq('[', ']'),
+    number: $ => /\d+(\.\d+)?/,
+    array: $ => seq('[', optional(seq($._value_expression, repeat(seq(',', $._value_expression)))), ']'),
     hash: $ => seq('{', optional(seq($.hash_pair, repeat(seq(',', $.hash_pair)))), '}'),
     hash_pair: $ => seq(field('key', choice($.identifier, $.string)), '=>', field('value', $._value_expression)),
     variable: $ => seq($.identifier, repeat(seq('.', $.identifier))),
-    identifier: $ => /[a-zA-Z0-9_][a-zA-Z0-9_]*/,
-    bare_string: $ => /[a-zA-Z0-9_.\/]+/,
+    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    path: $ => seq($.identifier, repeat1(choice('.', '/', $.identifier))),
     comparison_operator: $ => choice('==', '!=', '<', '<=', '>', '>='),
-    logical_op_high: $ => choice('&&', 'and'),
-    logical_op_low: $ => choice('||', 'or'),
+    logical_op_high: $ => choice('&&', 'AND', 'and'),
+    logical_op_low: $ => choice('||', 'OR', 'or'),
   }
 });
